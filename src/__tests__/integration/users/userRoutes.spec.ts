006b96049c7362fc4fbe843f1b700b31cd08c561
Igor Gomes <igorgomes255@gmail.com>
2023-01-30 22:09:12 -0300

    feat: adicionando testes

--- src/__tests__/integration/users/userRoutes.spec.ts
+++ src/__tests__/integration/users/userRoutes.spec.ts
@@ -121,4 +121,48 @@ describe("/users", () => {
     expect(userUpdated.body.full_name).toEqual("Kenzinho");
     expect(userUpdated.body).not.toHaveProperty("password");
   });
+
+  test("PATCH /users/profile -  should not be able to update user without authentication", async () => {
+    const loginResponse = await request(app)
+      .post("/login")
+      .send(mockedUserLogin);
+
+    const userToken = `Bearer ${loginResponse.body.token}`;
+
+    const response = await request(app)
+      .patch(`/users/profile`)
+      .set("Authorization", userToken);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(401);
+  });
+
+  test("DELETE /users/:id -  Must be able to soft delete user", async () => {
+    await request(app).post("/users").send(mockedUser);
+
+    const loginResponse = await request(app)
+      .post("/login")
+      .send(mockedUserLogin);
+
+    const response = await request(app)
+      .delete(`/users/profile`)
+      .set("Authorization", `Bearer ${loginResponse.body.token}`);
+
+    expect(response.status).toBe(204);
+  });
+
+  test("DELETE /users/profile -  should not be able to delete user without authentication", async () => {
+    const loginResponse = await request(app)
+      .post("/login")
+      .send(mockedUserLogin);
+
+    const userToken = `Bearer ${loginResponse.body.token}`;
+
+    const response = await request(app)
+      .delete(`/users/profile`)
+      .set("Authorization", userToken);
+
+    expect(response.body).toHaveProperty("message");
+    expect(response.status).toBe(401);
+  });
 });

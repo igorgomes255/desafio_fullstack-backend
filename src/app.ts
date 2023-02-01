025c414ac2718cfcc530c158c1027acbfd3627ee
Igor Gomes <igorgomes255@gmail.com>
2023-01-31 22:55:42 -0300

    feat: permitindo cors para usar no front

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,11 @@ import "express-async-errors";
 import handleErrorMiddleware from "./middlewares/handleError.middleware";
 import { appRoutes } from "./routes/routes";
 
+let cors = require("cors");
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 appRoutes(app);
 
